e0439cabfd2fa7a754e0d484e0c31445e8e34fb9
Irene <iheitsch@sbcglobal.net>
2018-08-12 12:10:03 -0500

    added classes and classroll tables, changed names of some constraints

--- createtables.sql
+++ createtables.sql
@@ -1,5 +1,7 @@
 DROP TABLE studentcheckins;
+Drop TABLE classroll;
 DROP TABLE students;
+Drop TABLE classes;
 DROP TABLE teacherlogins;
 DROP TABLE teachers;
 DROP TABLE allschools;
@@ -33,7 +35,16 @@ Values ('Cumminsky', 'Brian', 'brianc@yahoo.com', '67890', TRUE );
  TimeIn timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  TimeOut timestamp,
  PRIMARY KEY (ID),
- CONSTRAINT fk_teacherlogins_teachers FOREIGN KEY (teacherID) REFERENCES teachers (id) );
+ CONSTRAINT fk_teacherlogins_teachers
+  FOREIGN KEY (teacherID) REFERENCES teachers (id));
+
+# need to decide these on a case by case basis for each foreign key
+# ON DELETE NO ACTION); # won't let you delete a record that's referred to
+#                       # by a foreign key
+# ON DELETE SET NULL);  # sets foreign key to null if the key it refers to
+#                       # is deleted   
+# ON DELETE CASCADE);   # deletes any record that refers to foreign key
+#                       # if that key is deleted
 
 CREATE TABLE allschools (
 ID int NOT NULL AUTO_INCREMENT,
@@ -42,13 +53,25 @@ PRIMARY KEY (ID));
 
 INSERT INTO allschools (school) VALUES ('Blackshear'), ('Kealing'), ('Oak Springs'), ('Zavala');
 
+CREATE TABLE classes (
+ID int NOT NULL AUTO_INCREMENT,
+schoolID int,
+teacherID int,
+className varchar(30),
+PRIMARY KEY (ID),
+CONSTRAINT fk_classes_allschools
+  FOREIGN KEY (schoolID) REFERENCES allschools (ID) ON DELETE SET NULL,
+ CONSTRAINT fk_classes_teachers 
+  FOREIGN KEY (teacherID) REFERENCES teachers (ID) ON DELETE SET NULL);
+
 CREATE TABLE students (
 ID int NOT NULL AUTO_INCREMENT,
 LastName varchar(30),
 FirstName varchar(30),
 schoolID int,
 PRIMARY KEY (ID),
- CONSTRAINT fk_school FOREIGN KEY (schoolID) REFERENCES allschools (ID));
+ CONSTRAINT fk_students_allschools
+  FOREIGN KEY (schoolID) REFERENCES allschools (ID));
 
 INSERT INTO students (LastName, FirstName, schoolID) 
 VALUES('Smith','Dwayne', (SELECT ID FROM allschools WHERE school  =  'Oak Springs') );
@@ -57,20 +80,12 @@ VALUES('Sanchez','Delores', (SELECT ID FROM allschools WHERE school = 'Oak Sprin
 INSERT INTO students (LastName, FirstName, schoolID) 
 VALUES('Ramos', 'Sofia', (SELECT ID FROM allschools WHERE school = 'Zavala') );
  
-CREATE TABLE studentcheckins (
+create TABLE classroll (
  ID int NOT NULL AUTO_INCREMENT,
- studentID int,
- teacherID int,
- TimeIn timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
- TimeOut timestamp,
- broughtInstrument boolean,
- broughtBooks boolean,
- progress int,
- behavior int,
+ classID int NOT NULL,
+ studentID int NOT NULL,
  PRIMARY KEY (ID),
- CONSTRAINT fk_behavior FOREIGN KEY (behavior) REFERENCES fivenumbers (i),
- CONSTRAINT fk_progress FOREIGN KEY (progress) REFERENCES fivenumbers (i),
- CONSTRAINT fk_studentcheckins_students 
-  FOREIGN KEY (studentID) REFERENCES students (ID),
- CONSTRAINT fk_studentcheckins_teachers 
-  FOREIGN KEY (teacherID) REFERENCES teachers (ID));
+ CONSTRAINT fk_classroll_classes 
+  FOREIGN KEY (classID) REFERENCES classes (ID),
+ CONSTRAINT fk_class_roll_students 
+  FOREIGN KEY (studentID) REFERENCES students (ID));

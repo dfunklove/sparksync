f7207cb7fd85b6acb8de5b4404a55b7105d11dad
Dan Lovette <dlovette@gmail.com>
2018-08-30 18:03:09 -0500

    Add logout link to bottom of every page.  Remove page-specific link from lessons/checkout and lessons/new.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,11 +11,15 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
   # Logs out the current user.
   def log_out
     @login = Login.last_login session[:user_id]
     session.delete(:user_id)
-    if !@login.time_out
+    if @login && !@login.time_out
       @login.update_attributes( time_out: Time.zone.now )
     else
       puts "time out wasn't null"

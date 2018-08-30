5d5bff8e6c3de39b3207730d4827d8458893abd2
Irene Heitsch <iheitsch@sbcglobal.net>
2018-08-30 18:38:14 -0500

    added seeds.rb so i don't have to recreate teachers and schools, fixed a bug that was letting admins log is as teachers but not log out

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,80 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(  firstName: "Jane",
+  lastName: "Bennet",
+  email: "janeB@yahoo.edu",
+  password: "testjane",
+  password_confirmation: "testjane",
+  id: 9,
+  type: "Admin" )
+
+Admin.create!(  firstName: "William",
+  lastName: "Collins",
+  email: "billC@yahoo.edu",
+  password: "testbill",
+  password_confirmation: "testbill",
+  id: 13,
+  type: "Admin" )
+
+Teacher.create!(  firstName: "Charlotte",
+  lastName: "Lucas",
+  email: "charL@yahoo.edu",
+  password: "testchar",
+  password_confirmation: "testchar",
+  id: 35,
+  type: "Teacher" )
+
+Teacher.create!(  firstName: "Caroline",
+  lastName: "Bingley",
+  email: "caroB@yahoo.edu",
+  password: "testcaro",
+  password_confirmation: "testcaro",
+  id: 47,
+  type: "Teacher" )
+
+School.create!( name: "Blackshear")
+School.create!( name: "Kealing")
+School.create!( name: "Oak Springs")
+School.create!( name: "Zavala")
+
+Student.create!( firstName: "Byron",
+  lastName: "Watson",
+  school_id: 1 )
+
+Student.create!( firstName: "Lucy",
+  lastName: "Pevensie",
+  school_id: 4 )
+
+Student.create!( firstName: "Veruca",
+  lastName: "Salt",
+  school_id: 3 )
+
+Student.create!( firstName: "Dallas",
+  lastName: "Winston",
+  school_id: 2 )
+
+Student.create!( firstName: "Frank",
+  lastName: "Anderson",
+  school_id: 2 )
+
+Student.create!( firstName: "Ramona",
+  lastName: "Quimby",
+  school_id: 4 )
+
+Student.create!( firstName: "Rubeus",
+  lastName: "Hagrid",
+  school_id: 1 )
+
+Student.create!( firstName: "Fern",
+  lastName: "Arable",
+  school_id: 3 )
+
+Student.create!( firstName: "Harold",
+  lastName: "Johnson",
+  school_id: 3 )
+
+Student.create!( firstName: "Homer",
+  lastName: "Zuckerman",
+  school_id: 3 )

bd63d0472af0f797842c717b6fbdbeffef2db75f
Dan Lovette <dlovette@gmail.com>
2018-08-30 18:06:28 -0500

    Add in_progress method.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,8 @@ class Lesson < ApplicationRecord
   belongs_to :student
   belongs_to :teacher, class_name: 'Teacher', foreign_key: 'user_id'
   belongs_to :school
+
+  def self.in_progress
+  	Lesson.where(timeOut: nil)
+  end
 end

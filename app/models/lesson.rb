18f953a5394a36315c242998a5837db0b15af93c
Dan Lovette <dlovette@gmail.com>
2018-08-31 14:32:47 -0500

    Add student name, check in time, and current time to lessons/checkout

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,6 @@
 class Lesson < ApplicationRecord
+  DATETIME_FORMAT = "%Y-%m-%d %l:%M %p".freeze
+
   belongs_to :student
   belongs_to :teacher, class_name: 'Teacher', foreign_key: 'user_id'
   belongs_to :school

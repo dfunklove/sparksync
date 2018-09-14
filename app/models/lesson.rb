7848d1250d2d12d5ec0bd5af7b156c00cd4348e5
Dan Lovette <dlovette@gmail.com>
2018-09-14 14:24:24 -0500

    Lesson.in_progress only gives lessons from today.  Improved formatting of table.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,12 +1,13 @@
 class Lesson < ApplicationRecord
   DATETIME_FORMAT = "%Y-%m-%d %l:%M %p".freeze 
+  TIME_FORMAT = "%l:%M %p".freeze
 
   belongs_to :student
   belongs_to :teacher, optional: true, class_name: 'Teacher',foreign_key: 'user_id'
   belongs_to :school
 
   def self.in_progress
-  	Lesson.where(time_out: nil)
+  	Lesson.where(time_out: nil).where("time_in > ?", Time.new.beginning_of_day)
   end
 
 end

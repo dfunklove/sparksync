f4482e0984583b6aacb04b168a4934d258436882
Daniel Lovette <dlovette@gmail.com>
2019-06-12 01:13:16 -0500

    Omit lessons with deactivated student, teacher, or school.  When returning lessons for a student, teacher, or school, if the item is activated, omit lessons with other items that are deactivated.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -22,45 +22,63 @@ class Lesson < ApplicationRecord
     sql += "students.first_name, "
     sql += "students.last_name as student_last, user_id, student_id "
     sql += "from users inner join lessons on users.id = lessons.user_id "
-    sql += "inner join students on lessons.student_id = students.id "
-    sql += "inner join schools on lessons.school_id = schools.id "
+    sql += " inner join students on lessons.student_id = students.id "
+    sql += " inner join schools on lessons.school_id = schools.id "
     sql += "where time_out is not null"
     sql += " and ? <= time_in and time_in <= ? "
+    sql += " and students.activated = true"
+    sql += " and users.activated = true"
+    sql += " and schools.activated = true"
     self.find_by_sql([sql, start_date.to_s, end_date.to_s])
   end
 
   def self.find_by_school(school_id, start_date, end_date)
+    school = School.find(school_id)
     sql = "select users.first_name, users.last_name as teacher_last, "
     sql += "time_in, time_out, progress, behavior, notes, brought_instrument, "
     sql += "brought_books, students.first_name, "
     sql += "students.last_name as student_last, user_id, student_id "
-    sql += "from users inner join lessons on users.id = lessons.user_id"
-    sql += " inner join students on lessons.student_id = students.id where "
-    sql += " time_out is not null and lessons.school_id = " + school_id 
+    sql += "from users inner join lessons on users.id = lessons.user_id "
+    sql += " inner join students on lessons.student_id = students.id "
+    sql += "where time_out is not null and lessons.school_id = " + school_id 
     sql += " and ? <= time_in and time_in <= ? "
+    if school.activated?
+      sql += " and students.activated = true"
+      sql += " and users.activated = true"
+    end
     self.find_by_sql([sql, start_date.to_s, end_date.to_s])    
   end
 
   def self.find_by_student(student_id, start_date, end_date)
+    student = Student.find(student_id)
     sql = "select users.first_name, users.last_name as teacher_last, "
     sql += "time_in, time_out, progress, behavior, notes, brought_instrument, "
     sql += "brought_books, schools.name, user_id, student_id "
-    sql += "from users inner join lessons on users.id = lessons.user_id"
-    sql += " inner join students on lessons.student_id = students.id"
-    sql += " inner join schools on students.school_id = schools.id where "
-    sql += " time_out is not null and lessons.student_id = " + student_id 
+    sql += "from users inner join lessons on users.id = lessons.user_id "
+    sql += " inner join students on lessons.student_id = students.id "
+    sql += " inner join schools on students.school_id = schools.id "
+    sql += "where time_out is not null and lessons.student_id = " + student_id 
     sql += " and ? <= time_in and time_in <= ? "
+    if student.activated?
+      sql += " and users.activated = true"
+      sql += " and schools.activated = true"
+    end
     self.find_by_sql([sql, start_date.to_s, end_date.to_s])
   end
 
   def self.find_by_teacher(teacher_id, start_date, end_date)
+    teacher = Teacher.find(teacher_id)
     sql = "select name, time_in, time_out, progress, behavior, notes, "
     sql += "brought_instrument, brought_books, students.first_name, "
     sql += "students.last_name, student_id "
-    sql += "from schools inner join lessons on schools.id = lessons.school_id"
-    sql += " inner join students on lessons.student_id = students.id where "
-    sql += " time_out is not null and user_id = " + teacher_id 
+    sql += "from schools inner join lessons on schools.id = lessons.school_id "
+    sql += " inner join students on lessons.student_id = students.id "
+    sql += "where time_out is not null and user_id = " + teacher_id 
     sql += " and ? <= time_in and time_in <= ? "
+    if teacher.activated?
+      sql += " and students.activated = true"
+      sql += " and schools.activated = true"
+    end
     self.find_by_sql([sql, start_date.to_s, end_date.to_s])
   end
 end

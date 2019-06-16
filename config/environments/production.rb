04e9cf89bddd43e0af31fe4e3639d343c7daa237
Daniel Lovette <dlovette@gmail.com>
2019-06-16 00:48:11 -0500

    Set production URL for ActionMailer to sync.sparksforsuccess.org

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sparksync_#{Rails.env}"
   config.action_mailer.perform_caching = true
   config.action_mailer.delivery_method = :smtp
-  host = 'safe-sands-25204.herokuapp.com'
+  # If DNS breaks, app is still hosted at the server below
+  # host = 'safe-sands-25204.herokuapp.com'
+  host = 'sync.sparksforsuccess.org'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

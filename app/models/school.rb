11ffa2210007ab5b0d71a14cd99d8ecd76069081
Daniel Lovette <dlovette@gmail.com>
2019-07-07 01:32:10 -0500

    When deactivating a school, deactivate the students as well.  This prevents teachers having access to these students.

--- app/models/school.rb
+++ app/models/school.rb
@@ -4,4 +4,14 @@ class School < ApplicationRecord
   has_many :students
   has_many :lessons
   default_scope -> { order(name: :asc) }
+
+  after_update :update_students_activation, if :activated_changed?
+
+  private
+    def update_students_activation
+      # cascade activated value to all students at this school
+      # this prevents teachers having access to these students
+      self.students.update_all(activated: activated)
+    end
+  end
 end

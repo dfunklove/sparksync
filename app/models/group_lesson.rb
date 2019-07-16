9c8208741cb37694385ff987af82242abe1c0eb9
Daniel Lovette <dlovette@gmail.com>
2019-07-16 15:33:17 -0500

    Add teacher association

--- app/models/group_lesson.rb
+++ app/models/group_lesson.rb
@@ -1,5 +1,9 @@
 class GroupLesson < ApplicationRecord
   has_many :lessons, dependent: :destroy
+  belongs_to :teacher, class_name: 'Teacher',foreign_key: 'user_id'
+
+  validates :teacher, presence: true
+  validates_associated :lessons
 
   accepts_nested_attributes_for :lessons
 end

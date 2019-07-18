bbb56ad831ec5884235bd9c8c6aade290f80c8f8
Daniel Lovette <dlovette@gmail.com>
2019-07-18 02:07:32 -0500

    In group_lessons/new, Add Student button will create the student in the DB.  Adding a row to the form is not yet working.

--- app/controllers/group_lessons_controller.rb
+++ app/controllers/group_lessons_controller.rb
@@ -38,35 +38,94 @@ class GroupLessonsController < ApplicationController
   end
 
   def create
+    @confirm_add_student = false
     @group_lesson = GroupLesson.new(group_lesson_params)
     @group_lesson.teacher = current_user
     @group_lesson.time_in = Time.now
+    @selected = []
 
     # pick only the students/lessons which are selected
     params[:group_lesson][:lessons_attributes].keys.each do |key|
       lesson_data = params[:group_lesson][:lessons_attributes][key]
       if lesson_data["selected"]
-        @group_lesson.lessons << Lesson.new(lesson_params(lesson_data))
-      end
-      if !lesson_data[:student_id]
-        # process name and school id into student id
+        @selected[key] = true
+        lesson = Lesson.new(lesson_params(lesson_data))
+        lesson.teacher = current_user
+        lesson.time_in = @group_lesson.time_in
+        if !lesson.student_id
+          begin
+            @lesson = lesson
+            @student = Student.new(student_params lesson_data[:student])
+            @lesson.school_id = @student.school_id
+            @school = School.find(@student.school_id)
+          rescue => e
+            p e
+            @lesson ||= Lesson.new
+            @student ||= Student.new
+            @school ||= School.new
+          end
+          @lesson.student = @student
+          lookup_student_for_lesson
+        end
+        @group_lesson.lessons << lesson
       end
     end
 
-    @group_lesson.lessons.each do |lesson|
-      lesson.teacher = current_user
-      lesson.time_in = @group_lesson.time_in
-    end
-
-    puts "group_lesson.lessons.count=#{@group_lesson.lessons.count}, listing="
+    puts "group_lesson.lessons.size=#{@group_lesson.lessons.size}, listing="
     p @group_lesson.lessons
 
-    if @group_lesson.save
-      session[:group_lesson_id] = @group_lesson.id
-      redirect_to "/group_lessons/checkout"
-    else
-      prepare_new
-      render 'new'
+    respond_to do |format|
+      if params[:new_student]
+        lesson = Lesson.new
+        lesson.student = Student.new
+        lesson.student.school = School.new
+        @group_lesson.lessons << lesson
+        format.html { render action: 'new'}
+      elsif @group_lesson.errors.count == 0 && @group_lesson.save
+        session[:group_lesson_id] = @group_lesson.id
+        format.html { redirect_to "/group_lessons/checkout" }
+      elsif @confirm_add_student
+        format.html { render action: 'new'} # is this tested?
+        format.js { render 'confirm_add_student' }
+      else
+        format.html { render action: 'new'}
+        format.js # implied: render 'create'
+      end
+    end
+  end
+
+  def lookup_student_for_lesson
+    if @school.valid? && !@student.first_name.empty? && !@student.last_name.empty?
+      # if student exists in db get all the column values
+      # if student not in db prompt user to see if they want to create
+      harrys = Student.find_by_name(@student.first_name, @student.last_name, @school.id)
+      @stdnt_lookedup = harrys.first
+      nharrys = harrys.count
+
+      if @stdnt_lookedup
+   	    @lesson.student = @stdnt_lookedup
+        puts "number of students " + nharrys.to_s
+        if nharrys > 1
+          @lesson.errors.add(
+            :base,
+            :first_name_or_last_name_ambiguous,
+            message: "Need to spell out entire name")
+        end
+
+      elsif params[:new_student]
+        @student.school = @school
+        @student.activated = true
+        @student.save
+        @lesson.student = @student
+        flash[:info] = "Created new student #{@student.first_name} #{@student.last_name} at #{@student.school.name}" 
+      else
+        @lesson.errors.add(
+          :base,
+          :not_found_in_database,
+          message: 'No student by that name at that school. Check "new student" if you wish to add a new student to database, otherwise correct spelling or school')
+        @confirm_add_student = true
+      end
+      @lesson.school = @school
     end
   end
 
@@ -107,7 +166,7 @@ class GroupLessonsController < ApplicationController
     end
 
     def student_params params
-      params.require(:student).permit(:first_name, :last_name, :school_id)
+      params.permit(:first_name, :last_name, :school_id)
     end
 
     def teacher_user

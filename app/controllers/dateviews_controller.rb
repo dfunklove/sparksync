8b557b2ca7ee9484ee0df1ea4dcb9a8d99e77c8e
Daniel Lovette <dlovette@gmail.com>
2019-08-10 18:22:21 -0500

    Fixed bug where submitting the form on the "Date Errors" page would cause an exception due to bad URL.

--- app/controllers/dateviews_controller.rb
+++ app/controllers/dateviews_controller.rb
@@ -1,18 +1,22 @@
 class DateviewsController < ApplicationController
   def create
-    puts "dateview params start_date " + params[:dateview][:s_date_formatted]
-    puts "dateview params end_date " + params[:dateview][:e_date_formatted]
     @d = current_dateview # find the original or last stored dateview
     @d.s_date_formatted =  params[:dateview][:s_date_formatted] 
     @d.e_date_formatted = params[:dateview][:e_date_formatted]
-    puts "dateview errors " + @d.errors.messages.to_s
     if @d.errors.count == 0
-      # go back to the page where you changed date
-      # e.g. GET teacher/35 teachers/show.html.erb
       set_current_dateview @d
-      puts "going back to show teacher/id page"
-      redirect_to request.referrer
+
+      # go back to the page where user changed date
+      if session[:previous_page]
+        temp = session[:previous_page]
+        session[:previous_page] = nil
+        redirect_to temp
+      else
+        redirect_to request.referrer
+      end
+    else
+      session[:previous_page] = request.referrer
+      # show the update dateview/id page with form to correct date
     end
-    # show the update dateview/id page with form to correct date
   end
 end

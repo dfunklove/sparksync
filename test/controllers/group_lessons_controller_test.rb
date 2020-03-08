879f648c6e80236d645d539a4e3bdb5e7d844d25
Daniel Lovette <dlovette@gmail.com>
2020-03-08 12:12:42 -0500

    Add several new tests.  Login before each test.

--- test/controllers/group_lessons_controller_test.rb
+++ test/controllers/group_lessons_controller_test.rb
@@ -11,13 +11,18 @@ class GroupLessonsControllerTest < ActionDispatch::IntegrationTest
     retval
   end
 
-  @@once = false
+  def params_for_add_student(student, index)
+    retval = {}
+    retval["group_lesson[lessons_attributes][#{index}][student][first_name]"] = student.first_name
+    retval["group_lesson[lessons_attributes][#{index}][student][last_name]"] = student.last_name
+    retval["group_lesson[lessons_attributes][#{index}][student][school_id]"] = student.school.id
+    retval["group_lesson[lessons_attributes][#{index}][brought_instrument]"] = 1
+    retval["group_lesson[lessons_attributes][#{index}][brought_books]"] = 1
+    retval
+  end
 
   def setup
-    if !@@once
-      @@once = true
-      login
-   end
+    login
   end
 
   def login
@@ -37,10 +42,54 @@ class GroupLessonsControllerTest < ActionDispatch::IntegrationTest
     end
 
     count_before = GroupLesson.count
-    post "/group_lessons", params: params
+    post "/group_lessons", params: params, xhr: true
     assert_nil flash[:danger]
-    assert_redirected_to controller: :group_lessons, action: :checkout
+    assert_match "group_lessons/checkout", @response.body
+    assert_match /action.*replace/, @response.body
     assert_not_nil session[:group_lesson_id]
     assert_equal count_before + 1, GroupLesson.count
+  end
+
+  test "add existing student" do
+    params = params_for_add_student(Student.first, 0)
+    params["add_student"] = true
+    post "/group_lessons", params: params, xhr: true
+    assert_template :create
+  end
+
+  test "add new student without confirmation" do
+    s = Student.new
+    s.first_name = "Test"
+    s.last_name = "1"
+    s.school = School.first
+    params = params_for_add_student(s, 0)
+    params["add_student"] = true
+    post "/group_lessons", params: params, xhr: true
+    assert_template :confirm_add_student
+  end
+
+  test "add new student with confirmation" do
+    count_before = Student.count
+    s = Student.new
+    s.first_name = "Test"
+    s.last_name = "1"
+    s.school = School.first
+    params = params_for_add_student(s, 0)
+    params["add_student"] = true
+    params["new_student"] = true
+    post "/group_lessons", params: params, xhr: true
+    assert_template :create
+    assert_equal count_before + 1, Student.count
+  end
+
+  test "add new student without confirmation" do
+    s = Student.new
+    s.first_name = "Test"
+    s.last_name = "1"
+    s.school = School.first
+    params = params_for_add_student(s, 0)
+    params["add_student"] = true
+    post "/group_lessons", params: params, xhr: true
+    assert_template :confirm_add_student
   end
 end

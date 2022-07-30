09ac6a8e197d63359b8e6c36c6297a51e1bc7c69
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2022-07-29 21:55:54 -0500

    Rating Score should not be null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220730000257) do
+ActiveRecord::Schema.define(version: 20220730025235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20220730000257) do
   end
 
   create_table "ratings", force: :cascade do |t|
-    t.integer "score"
+    t.integer "score", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "goal_id"

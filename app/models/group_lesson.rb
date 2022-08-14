ca6efb057ffbf472995ec422280a96307a563dcd
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2022-08-14 00:58:46 -0500

    Remove reference to school_id because that column doesn't exist on this branch.

--- app/models/group_lesson.rb
+++ app/models/group_lesson.rb
@@ -8,15 +8,6 @@ class GroupLesson < ApplicationRecord
 
   accepts_nested_attributes_for :lessons
 
-  before_create :populate_school_id,:truncate_seconds
-
-  # Use the school_id from the first lesson (student)
-  def populate_school_id
-    if !self.school_id and !self.lessons.empty?
-      self.school_id = self.lessons[0].school_id
-    end
-  end
-
   # Reducing the granularity allows for a unique constraint to prevent duplicates
   def truncate_seconds
   	self.time_in = time_in.round

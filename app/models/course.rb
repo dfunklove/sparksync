b34267716a2d66683e31a89bf78f8cfbdbe9361a
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2022-08-23 21:57:52 -0500

    Validate for a minimum of 2 students.

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
   validates :school, presence: true
   validates :start_date, presence: true
   validates :teacher, presence: true
+  validates :students, length: { minimum: 2, message: "must number at least 2" }
 
   belongs_to :school
   belongs_to :teacher, class_name: 'Teacher',foreign_key: 'user_id'

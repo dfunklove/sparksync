5515abc842eff6863b299c4617ae0e6e46ed49d5
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2022-09-09 03:41:00 -0500

    Validate that start date is earlier than end date.

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,10 +4,17 @@ class Course < ApplicationRecord
   validates :start_date, presence: true
   validates :teacher, presence: true
   validates :students, length: { minimum: 2, message: "Please select two or more students" }
+  validate :start_date_before_end_date
 
   belongs_to :school
   belongs_to :teacher, class_name: 'Teacher',foreign_key: 'user_id'
 
   has_many :student_courses
   has_many :students, through: :student_courses
+
+  def start_date_before_end_date
+    if end_date <= start_date
+      errors.add(:start_date, "must be earlier than end date")
+    end
+  end
 end

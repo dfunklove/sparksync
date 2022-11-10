83ddfafabbe38206dba8fdb25db3c2b26474654c
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2022-11-09 18:25:11 -0600

    Change variable name back to changev

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -68,7 +68,7 @@ class StudentsController < ApplicationController
 
   def prepare_index
     @title = current_user.teacher? ? "My Students" : "Students"
-    @show_visibility_btn = current_visibility
+    @changev = current_visibility
     @students = find_right_students
     @isadmin = current_user.admin?
     @delete_warning = "Deleting this student will delete all his/her lessons records as well and is irreversible. Are you sure?"

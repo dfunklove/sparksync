9e3979a9d585655ebef6a93fbbcf71a1b2f8d442
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2023-01-01 23:20:20 -0600

    Validate presence of school_id

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,7 @@ class Student < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :last_name, uniqueness: {scope: [:first_name, :school_id] }
+  validates :school_id, presence: true
 
   belongs_to :school
   has_many :lessons
